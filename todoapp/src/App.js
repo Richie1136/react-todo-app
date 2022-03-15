35d8bb3da4f1ba79bc497024c2b6bbbe153adb9a
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-03-15 14:28:02 -0400

    Added a status state and a filteredTodos state to be able to filter based on what the status is

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -6,10 +6,12 @@ import { useState } from 'react'
 function App() {
   const [inputText, setInputText] = useState('')
   const [todos, setTodos] = useState([])
+  const [status, setStatus] = useState('all')
+  const [filteredTodos, setFilteredTodos] = useState([])
   return (
     <div className="App">
       <h1>Richie's Todo list</h1>
-      <Form todos={todos} setTodos={setTodos} inputText={inputText} setInputText={setInputText} />
+      <Form setStatus={setStatus} todos={todos} setTodos={setTodos} inputText={inputText} setInputText={setInputText} />
       <ToDolist setTodos={setTodos} todos={todos} />
     </div>
   );
--- todoapp/src/components/form/Form.js
+++ todoapp/src/components/form/Form.js
@@ -1,6 +1,6 @@
 import { v4 as uuid } from 'uuid'
 
-const Form = ({ setInputText, setTodos, todos, inputText }) => {
+const Form = ({ setInputText, setTodos, todos, inputText, setStatus }) => {
 
   const handleInputText = (event) => {
     setInputText(event.target.value)
@@ -15,6 +15,10 @@ const Form = ({ setInputText, setTodos, todos, inputText }) => {
     setInputText('')
   }
 
+  const handleStatus = (event) => {
+    setStatus(event.target.value)
+  }
+
   return (
     <form onSubmit={handleSubmit}>
       <input value={inputText} onChange={handleInputText} className="todo-input" type='text' />
@@ -22,7 +26,7 @@ const Form = ({ setInputText, setTodos, todos, inputText }) => {
         <i className="fas fa-plus-square"></i>
       </button>
       <div className="select">
-        <select className="filter-todo" name='todos'>
+        <select onChange={handleStatus} className="filter-todo" name='todos'>
           <option value='all'>All</option>
           <option value='completed'>Completed</option>
           <option value='uncompleted'>Uncompleted</option>
